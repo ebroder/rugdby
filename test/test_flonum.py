e15b5156280ad5474373e0495852f1a808de7eea
Evan Broder <evan@ebroder.net>
2014-12-28 18:37:44 +0100

    Include a flonum that will always show up as an RFloat

--- test/test_flonum.py
+++ test/test_flonum.py
@@ -11,3 +11,10 @@ class FlonumTest(gdbtest.GDBTest):
         rval = rugdby.RubyVALUE.from_value(val)
         self.assertIsInstance(rval, (rugdby.RubyFlonum, rugdby.RubyRFloat))
         self.assertPretty(val, '1.1')
+
+    def test_big_flonums(self):
+        val = gdb.parse_and_eval('rb_eval_string("1.72723e-77")')
+        rval = rugdby.RubyVALUE.from_value(val)
+        # This should always be an RFloat
+        self.assertIsInstance(rval, rugdby.RubyRFloat)
+        self.assertPretty(val, '1.72723e-77')
