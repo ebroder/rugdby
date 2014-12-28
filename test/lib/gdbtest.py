567fa56a3e4b1b07b7398bfe7598874768378f14
Evan Broder <evan@ebroder.net>
2014-12-28 19:10:43 +0100

    Get rid of a little spew

--- test/lib/gdbtest.py
+++ test/lib/gdbtest.py
@@ -8,16 +8,19 @@ import rugdby
 
 class GDBTest(unittest.TestCase):
     def setUp(self):
+        # If Python exceptions, print the whole stacktrace
         gdb.execute('set python print-stack full')
+        # Don't print lines about threads starting
+        gdb.execute('set print thread-events off')
 
         try:
             gdb.parse_and_eval('vm_exec_core')
-            bp = gdb.Breakpoint('vm_exec_core')
+            bp = gdb.Breakpoint('vm_exec_core', internal=True)
             bp.condition = 'th != 0'
         except gdb.error as e:
             if not e.args[0].startswith('No symbol'):
                 raise
-            bp = gdb.Breakpoint('ruby_exec')
+            bp = gdb.Breakpoint('ruby_exec', internal=True)
 
         bp.silent = True
 
