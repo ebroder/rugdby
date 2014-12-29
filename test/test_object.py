212690802ef5ef395bd25f40f69893eaddcdb3f9
Evan Broder <evan@ebroder.net>
2014-12-29 17:47:09 +0100

    Test more object corner cases

--- test/test_object.py
+++ test/test_object.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+import string
+
 import gdb
 import rugdby
 
@@ -9,3 +11,12 @@ class ObjectTest(gdbtest.GDBTest):
     def test_object_with_ivars(self):
         val = gdb.parse_and_eval("""rb_eval_string("class Test; end; x = Test.new; x.instance_variable_set(:@foo, 'bar'); x")""")
         self.assertPretty(val, "<Test @foo='bar'>")
+
+    def test_many_ivars(self):
+        val = gdb.parse_and_eval("""rb_eval_string("class Test; end; x = Test.new; (:@a..:@z).each {|k| x.instance_variable_set(k, 'bar')}; x")""")
+        ivars = ' '.join(['@%s=%r' % (c, 'bar') for c in string.ascii_lowercase])
+        self.assertPretty(val, "<Test %s>" % (ivars,))
+
+    def test_self_reference(self):
+        val = gdb.parse_and_eval("""rb_eval_string("class Test; end; x = Test.new; x.instance_variable_set(:@foo, x); x")""")
+        self.assertPretty(val, "<Test @foo=<...>>")
